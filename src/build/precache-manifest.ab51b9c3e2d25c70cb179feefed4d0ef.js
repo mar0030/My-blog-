2b8fbdf502116457b4897b0b0850b86f48ca05e9
Miguel Rubio <miyo_779@hotmail.com>
2020-05-10 08:44:03 -0500

    First commit

---
+++ src/build/precache-manifest.ab51b9c3e2d25c70cb179feefed4d0ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd886460bb75ff8ffd579857165425c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c428a535bd41d74a226",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "0cf6d6148858952f572d",
+    "url": "/static/js/2.f2b8de2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f2b8de2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c428a535bd41d74a226",
+    "url": "/static/js/main.87fd0f4b.chunk.js"
+  },
+  {
+    "revision": "c6cd80a6fce542ddaee9",
+    "url": "/static/js/runtime-main.b9244b71.js"
+  }
+]);
